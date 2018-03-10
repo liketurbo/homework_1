4b4f7cf2bd8b13d53daed5752dd1fe501a2b8a82
mr. One Two <mronetwo@localhost.localdomain>
2018-03-10 21:56:01 +0300

    made second door

--- src/js/doorOthers.js
+++ src/js/doorOthers.js
@@ -64,6 +64,71 @@ function Door1(number, onUnlock) {
 
     // ==== Напишите свой код для открытия второй двери здесь ====
     // Для примера дверь откроется просто по клику на неё
+    var animals = [
+        this.popup.querySelector('.door-riddle-anim__block_0'),
+        this.popup.querySelector('.door-riddle-anim__block_1'),
+        this.popup.querySelector('.door-riddle-anim__block_2'),
+        this.popup.querySelector('.door-riddle-anim__block_3'),
+        this.popup.querySelector('.door-riddle-anim__block_4'),
+        this.popup.querySelector('.door-riddle-anim__block_5'),
+        this.popup.querySelector('.door-riddle-anim__block_6'),
+        this.popup.querySelector('.door-riddle-anim__block_7'),
+        this.popup.querySelector('.door-riddle-anim__block_8')
+    ];
+
+    animals.forEach(function(el) {
+        el.addEventListener('pointerdown', _onButtonPointerDown.bind(this));
+    }.bind(this));
+
+    function _onButtonPointerDown(e) {
+        e.target.classList.add('door-riddle-anim__block_pressed');
+        currentSequence.push(_getId(e));
+        checkCondition(this);
+    }
+
+    var rightSequence = ['2', '1', '0', '3'];
+    var currentSequence = [];
+    function checkCondition(that) {
+        if (currentSequence.length === 4) {
+            if (_compareArrays(rightSequence, currentSequence)) {
+                that.unlock()
+            } else {
+                resetLevel();
+            }
+        }
+    }
+
+    function resetLevel() {
+        currentSequence = [];
+        animals.forEach(function(el) {
+            el.classList.remove('door-riddle-anim__block_pressed');
+        });
+    }
+
+    function _getId(e) {
+        return e.target.classList[1].slice(-1);
+    }
+
+    function _compareArrays(firstArr, secondArr) {
+        return firstArr.length == secondArr.length &&
+               firstArr.every(function(el, ind) { return el === secondArr[ind]});
+    }
+    // ==== END Напишите свой код для открытия второй двери здесь ====
+}
+Door1.prototype = Object.create(DoorBase.prototype);
+Door1.prototype.constructor = DoorBase;
+
+/**
+ * @class Door2
+ * @augments DoorBase
+ * @param {Number} number
+ * @param {Function} onUnlock
+ */
+function Door2(number, onUnlock) {
+    DoorBase.apply(this, arguments);
+
+    // ==== Напишите свой код для открытия третей двери здесь ====
+    // Для примера дверь откроется просто по клику на неё
 
     var targetBlocks = [
         this.popup.querySelector('.door-riddle-blocks__block_0-0'),
@@ -110,25 +175,35 @@ function Door1(number, onUnlock) {
         if (tappedButtons <= 1) return false;
         switch (_getId(e)) {
             case '0': case '1':
-                if (_inArea(0) && _inArea(1)) {
-                    _removeElem(0);
-                    _removeElem(1);
-                }
+            if (_inArea(0) && _inArea(1)) {
+                _removeElem(0);
+                _removeElem(1);
+            }
             case '2': case '3':
-                if (_inArea(2) && _inArea(3)) {
-                    _removeElem(2);
-                    _removeElem(3);
-                }
+            if (_inArea(2) && _inArea(3)) {
+                _removeElem(2);
+                _removeElem(3);
+            }
             case '0': case '2':
-                if (_inArea(0) && _inArea(2)) {
-                    _removeElem(0);
-                    _removeElem(2);
-                }
+            if (_inArea(0) && _inArea(2)) {
+                _removeElem(0);
+                _removeElem(2);
+            }
             case '1': case '3':
-                if (_inArea(0) && _inArea(2)) {
-                    _removeElem(0);
-                    _removeElem(2);
-                }
+            if (_inArea(0) && _inArea(2)) {
+                _removeElem(0);
+                _removeElem(2);
+            }
+            case '0': case '3':
+            if (_inArea(0) && _inArea(3)) {
+                _removeElem(0);
+                _removeElem(3);
+            }
+            case '1': case '2':
+            if (_inArea(1) && _inArea(2)) {
+                _removeElem(1);
+                _removeElem(2);
+            }
         }
 
         var count = document.querySelector('.door-riddle-blocks__drag-blocks').children.length;
@@ -153,17 +228,17 @@ function Door1(number, onUnlock) {
     function _removeElem(id) {
         switch (id) {
             case 0:
-                dragBlocks[0].parentNode.removeChild(dragBlocks[0]);
-                break;
+            dragBlocks[0].parentNode.removeChild(dragBlocks[0]);
+            break;
             case 1:
-                dragBlocks[1].parentNode.removeChild(dragBlocks[1]);
-                break;
+            dragBlocks[1].parentNode.removeChild(dragBlocks[1]);
+            break;
             case 2:
-                dragBlocks[2].parentNode.removeChild(dragBlocks[2]);
-                break;
+            dragBlocks[2].parentNode.removeChild(dragBlocks[2]);
+            break;
             case 3:
-                dragBlocks[3].parentNode.removeChild(dragBlocks[3]);
-                break;
+            dragBlocks[3].parentNode.removeChild(dragBlocks[3]);
+            break;
         }
     }
 
@@ -179,25 +254,6 @@ function Door1(number, onUnlock) {
 
         return (diffX <= (width / 2) && diffY <= (height / 2));
     }
-    // ==== END Напишите свой код для открытия второй двери здесь ====
-}
-Door1.prototype = Object.create(DoorBase.prototype);
-Door1.prototype.constructor = DoorBase;
-
-/**
- * @class Door2
- * @augments DoorBase
- * @param {Number} number
- * @param {Function} onUnlock
- */
-function Door2(number, onUnlock) {
-    DoorBase.apply(this, arguments);
-
-    // ==== Напишите свой код для открытия третей двери здесь ====
-    // Для примера дверь откроется просто по клику на неё
-    this.popup.addEventListener('click', function() {
-        this.unlock();
-    }.bind(this));
     // ==== END Напишите свой код для открытия третей двери здесь ====
 }
 Door2.prototype = Object.create(DoorBase.prototype);
